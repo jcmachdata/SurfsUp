98b0fcc64a0d4de8b6e29c719e4c25b99ac771a9
John Chambers <chambers216@gmail.com>
2019-04-02 19:12:56 -0500

    commit

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, jsonify
 import sqlalchemy
-##from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
